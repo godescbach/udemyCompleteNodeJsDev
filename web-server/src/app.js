6ff4a5f0cf6774346a37216487e7e442c98313d4
Tom Mostyn <godescbach@gmail.com>
2019-12-09 07:35:36 -0600

    Adding Express.js to weather-app.

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,25 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, resp) => {
-  resp.send('Hello express!')
+  resp.send('<h1>Weather</h1>')
 })
 
 app.get('/help', (req, resp) => {
-  resp.send('Help page')
+  resp.send({
+    name: 'Tom',
+    age: 53,
+  })
 })
 
 app.get('/about', (req, resp) => {
-  resp.send('Weather App 1.0')
+  resp.send('<h1>Weather App 1.0</h1>')
 })
 
 app.get('/weather', (req, resp) => {
-  resp.send('Weather page')
+  resp.send({
+    location: 'Dallas',
+    forecast:  'It\'s Texas in December, so comfortable 70 degrees.'
+  })
 })
 
 // app.com
