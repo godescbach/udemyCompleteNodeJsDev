e2437ca7addc28ada188839bc3eae09ecef6ffeb
Tom Mostyn <godescbach@gmail.com>
2019-12-10 19:01:28 -0600

    Replaced main, help and about pages using hbs, handlebars for express, for dynamic content.

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,11 +4,30 @@ const express = require('express')
 
 const app = express()
 
+app.set('view engine', 'hbs')
 app.use(express.static(path.join(__dirname, '../public')))
 
-// app.get('', (req, resp) => {
-//   resp.send('<h1>Weather</h1>')
-// })
+app.get('', (req, resp) => {
+  resp.render('index', {
+    title: 'Weather',
+    name: 'Tom Mostyn III'
+  })
+})
+
+app.get('/about', (req, resp) => {
+  resp.render('about',  {
+    title: 'About Me',
+    name: 'Tom Mostyn III'
+  })
+})
+
+app.get('/help', (req, resp) => {
+  resp.render('help',  {
+    title: 'Help',
+    message: 'This is my help page message.'
+  })
+})
+
 
 // app.get('/help', (req, resp) => {
 //   resp.send({
