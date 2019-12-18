bea52c1c400c0bf222592a94f96d4500e5ed7876
Tom Mostyn <godescbach@gmail.com>
2019-12-18 05:35:07 -0600

    Finished weather-app.  Connected form & button to nodejs input and styled app.

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -6,12 +6,39 @@ console.log('Client side javascript file is loaded!')
 //   })
 // })
 
-fetch('http://localhost:3000/weather?address=Boston').then((response) => {
+// fetch('http://localhost:3000/weather?address=Boston').then((response) => {
+//   response.json().then((data) => {
+//     if(data.error) {
+//       console.log(data.error)
+//     } else {
+//       console.log(data)
+//     }
+//   })
+// })
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+messageOne.textContent = 'Loading ...'
+messageTwo.textContent = ''
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const location = search.value
+  console.log(location)
+  fetch('http://localhost:3000/weather?address=' + location).then((response) => {
   response.json().then((data) => {
     if(data.error) {
-      console.log(data.error)
+      // console.log(data.error)
+      messageOne.textContent = data.error
+      messageTwo.textContent = ''
     } else {
-      console.log(data)
+      messageOne.textContent = data.location
+      messageTwo.textContent = data.forecast
+      // console.log(data)
     }
   })
+})
 })
